1d2c053a879925e75b6700535fcbec82cf73b1b9
Marko Baskarad <marko.baskarad34@gmail.com>
2020-12-11 12:37:59 +0100

    add another class component with different content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     <div className="App">
       My Hello World!
       <FirstComponent />
+      <SecondComponent />
     </div>
   );
  }
@@ -22,4 +23,15 @@ class FirstComponent extends Component {
   );
  }
 }
+
+class SecondComponent extends Component {
+  render() {
+   return (
+    <div className="secondComponent">
+      SecondComponent
+    </div>
+  );
+ }
+}
+
 export default App;
