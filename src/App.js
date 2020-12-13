b4646bbb3c4c3f06a5472398508ed286fbd5049e
Marko Baskarad <marko.baskarad34@gmail.com>
2020-12-13 17:29:21 +0100

    create  initial setup for counter  app, implement onClick function and state and update state also

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {Component} from "react";
-import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from'./components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent'
-
+import React, { Component } from "react";
+import "./App.css";
+import FirstComponent from "./components/learning-examples/FirstComponent";
+import SecondComponent from "./components/learning-examples/SecondComponent";
+import ThirdComponent from "./components/learning-examples/ThirdComponent";
+import Counter from "./components/counter/Counter";
 class App extends Component {
   render() {
-   return <div className="App">
-      My Hello World!
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-      <FourthComponent></FourthComponent>
-    </div>
-  ;
- }
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
 }
 
-function FourthComponent() {
-
-  return (
-    <div className="fourthComponent">
-        FourthComponent
-    </div>
-  );
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
+        My Hello World!
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./Counter.css";
+
+class Counter extends Component {
+  //Define the initial state in constructor
+  //state => counter 0
+  constructor() {
+    super(); //Error1
+    this.state = {
+      counter: 0,
+    };
+    //If we want use this in method in react js we must bind this
+    //to inncrement method in class
+    this.increment = this.increment.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}>+1</button>
+        <span className="count">{this.state.counter}</span>
+      </div>
+    );
+  }
+
+  increment() {
+    //Update state - counter++
+    //console.log("increment");
+
+    //If we want use this in method in  react js we must bind this
+    //to inncrement method in class
+
+    //update state via setState not directly this.state.counter++;Bad practice
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+}
+
+export default Counter;
