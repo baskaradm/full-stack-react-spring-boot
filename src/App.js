db287f6203b64d295759c807bf91f10fd3f0d7e4
Marko Baskarad <marko.baskarad34@gmail.com>
2021-01-05 18:58:15 +0100

    change prop type in his initialization

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter by="1" />
+        <Counter by={1} />
         <Counter />
         <Counter by={5} />
         <Counter by={10} />
