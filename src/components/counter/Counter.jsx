a9f39085385d99cb931a07e553966c8a041fb9c2
Marko Baskarad <marko.baskarad34@gmail.com>
2021-01-05 21:38:46 +0100

    move CounterButton component in separate module

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Counter.css";
-import PropTypes from "prop-types";
+import CounterButton from "../counter/CounterButton";
 class Counter extends Component {
   render() {
     return (
@@ -13,46 +13,4 @@ class Counter extends Component {
   }
 }
 
-class CounterButton extends Component {
-  //Define the initial state in constructor
-  //state => counter 0
-  constructor() {
-    super(); //Error1
-    this.state = {
-      counter: 0,
-    };
-    //If we want use this in method in react js we must bind this
-    //to inncrement method in class
-    this.increment = this.increment.bind(this);
-  }
-  //When you use arrow function you don't need to binding this variable
-  //because this is binding automatically
-  render() {
-    return (
-      <div className="counter">
-        <button onClick={this.increment}>+{this.props.by}</button>
-        <span className="count">{this.state.counter}</span>
-      </div>
-    );
-  }
-  increment() {
-    //Update state - counter++
-    //console.log("increment");
-
-    //If we want use this in method in  react js we must bind this
-    //to inncrement method in class
-
-    //update state via setState not directly this.state.counter++;Bad practice
-    this.setState({
-      counter: this.state.counter + this.props.by,
-    });
-  }
-}
-CounterButton.defaultProps = {
-  by: 1,
-};
-
-CounterButton.propTypes = {
-  by: PropTypes.number,
-};
 export default Counter;
