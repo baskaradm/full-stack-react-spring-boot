6cdff0375e99a7e534d58171e362ba372657ed4a
Marko Baskarad <marko.baskarad34@gmail.com>
2021-01-12 18:24:16 +0100

    remove state and methods from child component

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -2,29 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class CounterButton extends Component {
-  //Define the initial state in constructor
-  //state => counter 0
-  constructor() {
-    super(); //Error1
-    this.state = {
+  /*  constructor() {
+    super();  
+      this.state = {
       counter: 0,
     };
-    //If we want use this in method in react js we must bind this
-    //to inncrement method in class
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
+   
+     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this); 
   }
-  //When you use arrow function you don't need to binding this variable
-  //because this is binding automatically
+  */
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+{this.props.by}</button>
-        <button onClick={this.decrement}>-{this.props.by}</button>
+        <button onClick={() => this.props.incrementMethod(this.props.by)}>
+          +{this.props.by}
+        </button>
+        <button onClick={() => this.props.decrementMethod(this.props.by)}>
+          -{this.props.by}
+        </button>
       </div>
     );
   }
-  increment() {
+  /* increment() {
     this.setState((prevState) => {
       return { counter: prevState.counter + this.props.by };
     });
@@ -35,7 +35,7 @@ class CounterButton extends Component {
       return { counter: prevState.counter - this.props.by };
     });
     this.props.decrementMethod(this.props.by);
-  }
+  } */
 }
 CounterButton.defaultProps = {
   by: 1,
