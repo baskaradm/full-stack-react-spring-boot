39ecbd4cf3155fac9c24156da030717242559386
Marko Baskarad <marko.baskarad34@gmail.com>
2021-01-12 12:57:40 +0100

    using prevState in setState as parameter to arrow function to update the state

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -22,8 +22,8 @@ class Counter extends Component {
   }
   increment(by) {
     //console.log(`increment from child ${by}`);
-    this.setState({
-      counter: this.state.counter + by,
+    this.setState((prevState) => {
+      return { counter: prevState.counter + by };
     });
   }
 }
